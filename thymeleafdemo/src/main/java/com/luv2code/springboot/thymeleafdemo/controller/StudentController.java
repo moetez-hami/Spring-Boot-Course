d37a34619e997dd91d772ccd12e777c02f550296
moetez-hami <65268618+moetez-hami@users.noreply.github.com>
2024-03-07 05:11:29 +0100

    Add some form Elements and Data binding

--- thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,15 @@ import com.luv2code.springboot.thymeleafdemo.model.Student;
 
 @Controller
 public class StudentController {
+	
+	@Value("${countries}")
+	private List<String> countries;
+	
+	@Value("${languages}")
+	private List<String> languages;
+	
+	@Value("${systems}")
+	private List<String> systems;
 
 	@GetMapping("showStudentForm")
 	public String showForm(Model theModel) {
@@ -19,6 +31,16 @@ public class StudentController {
 		
 		// add student object to the model
 		theModel.addAttribute("student",theStudent);
+		
+		// add the list of countries to the model
+		theModel.addAttribute("countries", countries) ;
+		
+		// add the list of languages to the model
+		theModel.addAttribute("languages", languages);
+		
+		// add the list of systems to the model
+		theModel.addAttribute("systems", systems);
+		
 		return "student-form";
 	}
 	
